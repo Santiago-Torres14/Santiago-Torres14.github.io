18731842918ea9e9d0965bd045f6860c80714d7f
Santiago Torres Aragon <santiagotorres@freedomtorres.tech>
2022-12-31 16:29:22 -0600

    Attempt #4 to fix dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://santiagotorres.me',
-  integrations: [mdx()]
 });
